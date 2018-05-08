5d492a9adc96a3d4771952b5239944b416351811
Facundo Mainere <fmmainere@gmail.com>
2018-05-08 14:42:33 -0300

    changed execution time

--- consol-scrapper/scheduler.py
+++ consol-scrapper/scheduler.py
@@ -3,7 +3,7 @@ import main
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=13)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=1)
 def scheduled_job():
     main.update_products()
 
