d601e81ce1abd4a88477dd083ab9c8d86f41b6d4
Damian Mantuano <smarbos@gmail.com>
2018-05-08 12:26:31 -0300

    [DM] Added styles and ascii vampire, offcourse

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,10 @@
 <template>
   <v-card>
     <v-card-title>
-      Productos Consol ლ(╹◡╹ლ)
+      <div class="header">
+        <img src="static/camba-logo.png">
+        Productos Consol <span class="carita"><span class="manitos">ლ</span>(╹◡╹<span class="manitos">ლ</span>)</span>
+      </div>
       <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
@@ -53,21 +56,24 @@ export default {
       headers: [
         {
           text: 'Producto',
-          align: 'left',
+          align: 'center',
           sortable: true,
-          value: 'producto'
+          value: 'producto',
+          width: '70%'
         },
         {
           text: 'Precio (AR$)',
-          align: 'left',
+          align: 'right',
           sortable: true,
-          value: 'precio'
+          value: 'precio',
+          width: '15%'
         },
         {
           text: 'Agregar al carrito',
-          align: 'left',
+          align: 'right',
           sortable: false,
-          value: 'carrito'
+          value: 'carrito',
+          width: '15%'
         }
       ],
       products: []
@@ -78,6 +84,77 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+                              /* v.._                       _..v
+                               )\ '-.                 .-' /(
+                               : \   :               :   / :
+                                '.\   '.    ._.    .''  /.'
+                                  :\    i.-'   '-.i    /:
+                                   '\ .-'         '-. /'
+_..eeaa.._                          :                :                          _..eeaa.._
+i"T$P^T888aaa.._                   /   .-'\. ./'-.   \                    _..eee8887^T$P"i
+'.     "T888888aa.               /   /  ()/ \()  \   \                .ee8888887"     .'
+  '      '"T8888.88a            :    '._.' . '._.'    :             e88.88887"'      '
+  i           "T8888a.          | .i'     / \     'i. |          .e8888P"           i
+  :             "T8888a.        |  '.     '-'     .'  |        .e88867"             :
+   i               T8888a.      :    i__..-=-..__i    :      .e8888P               i
+  :                "T8888a.     '.   \/       \/   .'     .e8888P"                :
+  :                  "88888a.     i-.           .-i     .e88888"                  :
+  i                  "T88888a._./   '-..___..-'   \._.e88888P"                  i
+  :                     "T88888P                   Y88888P"                     :
+  :                       'T88"                     "88P'                       :
+  i                    .__.:                         :.__.                    i
+  :               _.--'   :                           :   '--._               :
+  :         _.--'        |          C A M B A         |        '--._         :
+  i___...--'             :            2018           :             '--...___i
+                          :                         :
+                          '.                       .'
+                           '.                     .'
+                             ''                 .' .
+                              Y'-..         ..-'Y
+                             :     '-.___.-'     :
+                             |      .'87"'.      |
+                             :     :  ^.   :     :
+                              :   :   "^.   :   :        ._.
+                              |   |    "^.  |   |        )&(
+                              :   :      "'e:   :    .aP" ^
+                               : :           : : "TP"
+                               | |           | |
+                               : :           : :
+                              /   )         (   \          fsc
+                           _.'   /           \   '._
+                         .' _ . /             \     '.
+                        (_i-i/_i               i_\i-i_)                                */
+
+
+
+.header img {
+  display: block;
+  margin-bottom: 5px;
+}
+.header {
+  position: relative;
+  display: block;
+}
+.carita {
+  font-weight: bolder;
+}
+.manitos {
+  animation: MoveUpDown 1s linear infinite;
+  position: relative;
+  left: 0;
+  bottom: 0;
+}
+
+@keyframes MoveUpDown {
+  0%, 100% {
+    bottom: 0;
+  }
+  50% {
+    bottom: 5px;
+  }
+}
+
 h1, h2 {
   font-weight: normal;
 }
@@ -92,4 +169,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
